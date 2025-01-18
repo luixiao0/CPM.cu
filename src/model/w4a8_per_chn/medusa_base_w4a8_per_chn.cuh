90fd50ce3db6eec76b5da06b390aff0563d52813
YudiZh <yvdi.zhang@outlook.com>
2025-01-18 09:53:36 +0800

    w4a8 model implementation done

---
+++ src/model/w4a8_per_chn/medusa_base_w4a8_per_chn.cuh
@@ -0,0 +1,104 @@
+#pragma once
+#include "w4a8_per_chn_model.cuh"
+#include "../medusa.cuh"
+
+
+template<typename T>
+struct MedusaImplBaseW4A8PerChn : Model {
+    int num_heads;
+    int num_layers;
+
+    W4A8PerChnModelImpl<T>* model;
+    std::vector<ResidualBlock<T>*> blocks;
+    std::vector<Linear<T>*> lm_heads;
+
+    T* last_token_hidden_state;
+    int32_t *h_best, *d_best;
+
+    T* tmp_kvcache;
+
+    MedusaImplBaseW4A8PerChn(
+        W4A8PerChnModelImpl<T>* model,
+        int num_heads,
+        int num_layers
+    ) {
+        this->model = model;
+        this->num_heads = num_heads;
+        this->num_layers = num_layers; // asserted in python that num_layers == 1
+
+        for (int i = 0; i < num_heads; i++) {
+            blocks.push_back(new ResidualBlock<T>(model->hidden_size, model->hidden_size));
+            lm_heads.push_back(new Linear<T>(model->hidden_size, model->vocab_size));
+        }
+    }
+
+    void init_weight_ptr(Memory* memory) {
+        for (int i = 0; i < num_heads; i++) {
+            blocks[i]->init_weight_ptr(memory);
+            lm_heads[i]->init_weight_ptr(memory);
+        }
+    }
+
+    int64_t init_output_ptr(Memory* memory, int32_t num_tokens, int64_t offset) {
+        for (int i = 0; i < num_heads; i++) {
+            offset = blocks[i]->init_output_ptr(memory, num_tokens, offset);
+            // lm_head do not allocate, directly output
+        } 
+        offset = memory->allocate((void**)&d_best, offset, 2 * sizeof(int32_t));
+        cudaMallocHost(&h_best, 2 * sizeof(int32_t));
+        offset = memory->allocate((void**)&tmp_kvcache, offset, 64 * this->model->kv_caches->num_hidden_layers * 2 * this->model->kv_caches->dim * sizeof(T));
+        return offset;
+    }
+
+    int init_storage() {
+        this->model->init_weight_ptr(this->model->memory);
+        this->init_weight_ptr(this->model->memory);
+        int64_t offset = this->model->init_output_ptr(this->model->memory, this->model->chunk_length, this->model->memory->model_offset);
+        int64_t kv_cache_offset = this->init_output_ptr(this->model->memory, 1, offset);
+        this->model->init_kv_cache(kv_cache_offset);
+        return this->model->kv_caches->budget;
+    }
+
+    void load_to_storage(std::string name, void* ptr) {
+        if (name.substr(0, 6) == "medusa") {
+            std::regex layer_regex("medusa\\.(\\d+)\\.(\\d+).*");
+            std::smatch matches;
+            if (std::regex_search(name, matches, layer_regex)) {
+                int head_idx = std::stoi(matches[1]);
+                int layer_idx = std::stoi(matches[2]);
+                if (layer_idx == 0) {
+                    blocks[head_idx]->load_to_storage(name, ptr);
+                } else {
+                    lm_heads[head_idx]->load_to_storage(name, ptr);
+                }
+            }
+        } else {
+            this->model->load_to_storage(name, ptr);
+        }
+    }
+
+    void prefill(int32_t num_tokens, int32_t num_history_tokens, int32_t* input, int32_t* position_ids, void* output) {
+        this->model->prefill(num_tokens, num_history_tokens, input, position_ids, output);
+        this->last_token_hidden_state = this->model->norm->output + (num_tokens - 1) * this->model->hidden_size;
+    }
+
+    void decode(int32_t num_tokens, int32_t padded_length, int32_t* input, int32_t* position_ids, int32_t* cache_length, uint64_t* mask_2d, void* output) {
+        this->model->decode(num_tokens, padded_length, input, position_ids, cache_length, mask_2d, output);
+    }
+
+    void draft(void* output) {
+        for (int i = 0; i < num_heads; i++) {
+            blocks[i]->prefill(1, this->last_token_hidden_state);
+            lm_heads[i]->prefill(1, blocks[i]->output, (T*)output + i * this->model->vocab_size);
+        }
+    }
+
+    int verify(int32_t num_tokens, int32_t* pred, int32_t* gt, int32_t* position_ids, int32_t* cache_length, uint64_t* attn_mask, int32_t* tree_parent) {
+        verify_kernel<<<1, 64, 0, calc_stream>>>(num_tokens, pred, gt, position_ids, cache_length, attn_mask, tree_parent, d_best);
+        cudaMemcpyAsync(h_best, d_best, 2 * sizeof(int32_t), cudaMemcpyDeviceToHost, calc_stream);
+        cudaStreamSynchronize(calc_stream);
+        fix_kv_cache(h_best[0], this->model->kv_caches->num_hidden_layers * 2, this->model->kv_caches->dim, pred, gt, cache_length, this->model->kv_caches->d_flat_caches, this->tmp_kvcache);
+        this->last_token_hidden_state = this->model->norm->output + h_best[1] * this->model->hidden_size;
+        return h_best[0];
+    }
+};
--- src/model/w8a8/fused_kernel.cuh
+++ src/model/w8a8/fused_kernel.cuh
@@ -45,6 +45,54 @@ __global__ void quant_kernel(T *__restrict__ input,
     }
   }
 }
+
+template <typename T, typename scale_type, bool use_per_token_quant>
+__global__ void quant_kernel_fuse_sum(T *__restrict__ input,
+                             int8_t *__restrict__ output, 
+                             scale_type input_sum, 
+                             scale_type scale,
+                             int num_tokens, int hidden_size) {
+  // TODO: get the sum here.
+  const int tid = threadIdx.x;
+  const int token_idx = blockIdx.x;
+  const int64_t token_idx_mul_hidden_size = token_idx * int64_t(hidden_size);
+
+  if constexpr (use_per_token_quant) {
+    float amax_val = 0.0f;
+    float sum_val = 0.0f;
+    const float zero = 0.0f;
+
+    for (int i = tid; i < hidden_size; i += blockDim.x) {
+      float val = (float)input[token_idx_mul_hidden_size + i];
+      sum_val += val;
+      val = val > zero ? val : -val;
+      if (val > amax_val)
+        amax_val = val;
+    }
+
+    __shared__ float s_amax;
+    const float block_amax_val = blockReduceMax(amax_val);
+    const float block_sum_val = blockReduceSum(sum_val);
+    if (tid == 0) {
+      s_amax = block_amax_val;
+      scale[token_idx] = __float2half_rn(block_amax_val / 127.0f);
+      input_sum[token_idx] = __float2half_rn(block_sum_val);
+    }
+    __syncthreads();
+
+    float tmp_scale = 127.0f / s_amax;
+    for (int i = tid; i < hidden_size; i += blockDim.x) {
+      output[token_idx_mul_hidden_size + i] =
+          float_to_int8_rn(((float)input[token_idx_mul_hidden_size + i]) * tmp_scale);
+    }
+  } else {
+    for (int i = tid; i < hidden_size; i += blockDim.x) {
+      output[token_idx_mul_hidden_size + i] =
+          float_to_int8_rn(((float)input[token_idx_mul_hidden_size + i]) / __half2float(scale));
+    }
+  }
+}
+
 }
 
 template <typename T, bool use_per_token_quant>
@@ -77,5 +125,50 @@ struct Quantizer
 
         quant_kernel<T, half *, true><<<grid, block, 0, calc_stream>>>(input, output, output_scale, num_tokens, dim);
 
+    }
+};
+
+template <typename T, bool use_per_token_quant>
+struct QuantizerFuseSum
+{
+    int dim;
+    int8_t* output;
+    // using ScaleType = typename std::conditional<use_per_token_quant, half *, half>::type;
+    half* output_scale;
+    half* output_sum;
+
+    QuantizerFuseSum(int dim)
+    {
+        this->dim = dim;
+    }
+
+    int64_t init_output_ptr(Memory * memory, int32_t num_tokens, int64_t offset){
+        return memory->allocate((void**)&this->output, offset, num_tokens * dim * sizeof(int8_t));
+    }
+
+    int64_t init_output_scale_and_sum_ptr(Memory * memory, int32_t num_tokens, int64_t offset){
+        int64_t scale_offset = memory->allocate((void**)&this->output_scale, offset, num_tokens * sizeof(half));
+        return memory->allocate((void**)&this->output_sum, scale_offset, num_tokens * sizeof(half));
+    }
+
+    void invoke(T *input,
+                int num_tokens)
+    {
+        // 配置线程块和线程网格
+        dim3 grid(num_tokens);
+        dim3 block(std::min(dim, 1024));
+
+        // 调用核函数
+        // quant_kernel<T, half *, true><<<grid, block, 0, calc_stream>>>(input, output, scale, num_tokens, dim);
+        quant_kernel_fuse_sum<T, half *, true><<<grid, block, 0, calc_stream>>>(
+        input, output, output_sum, output_scale, num_tokens, dim);
+
+        // 检查内核调用的错误
+        // cudaError_t err = cudaGetLastError();
+        // if (err != cudaSuccess)
+        // {
+        //     printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
+        //     assert(false);
+        // }
     }
 };
