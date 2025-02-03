b37b7000454b6d9fc020bae5b50438e27e9c1e52
YudiZh <yvdi.zhang@outlook.com>
2025-02-04 01:17:08 +0800

    w4a16 test scripts

---
+++ tests/test_generate_w4a16.py
@@ -0,0 +1,81 @@
+import os
+import torch
+from llamacu.llama_w4a16 import W4A16LLM
+from llamacu.speculative.medusa_base_w4a16 import W4A16LLM_with_medusa
+from llamacu.speculative.medusa_choices import *
+from llamacu.speculative.eagle_base_w4a16 import W4A16LLM_with_eagle
+# from llamacu.medusa_w8a8 import W8A8LLM_with_medusa
+from transformers import AutoTokenizer
+from triton.testing import do_bench
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# path = "../../models/MiniCPM-1B-sft-llama-format"
+# path = "../../models/Llama-2-7b-hf"
+# path = "/home/ydzhang/checkpoints/deepcompress/Meta-Llama-3-8B-Instruct-w4a8-gchn-pileval"
+# path = "/home/ydzhang/checkpoints/MaziyarPanahi/Meta-Llama-3-8B-Instruct-GPTQ"
+# path = "/home/ydzhang/checkpoints/AutoGPTQ/Meta-Llama-3-8B-Instruct-4bit-128g-pileval-mse_shrink0.4"
+path = "/home/ydzhang/checkpoints/GPTQ/Meta-Llama-3-8B-Instruct-4bit-128g-pileval-marlin"
+# quantized_model_dir = "/home/ydzhang/checkpoints/GPTQ/Meta-Llama-3-8B-Instruct-4bit-128g-marlin"
+medusa_path = "/home/ydzhang/checkpoints/predibase/Meta-Llama-3-8B-Instruct-medusa-full"
+eagle_path = "/home/ydzhang/checkpoints/yuhuili/EAGLE-LLaMA3-Instruct-8B"
+dtype = torch.float16
+cuda_graph = False
+num_generate = 512
+model_type = ["base", "medusa", "eagle"][2]
+Bench = True
+
+# prompt = "Beijing is the"
+
+messages = [
+            {"role": "system",
+             "content": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."},
+        ]
+
+question = "Compose an engaging travel blog post about a recent trip to Hawaii, highlighting cultural experiences and must-see attractions."
+
+messages.append({
+                "role": "user",
+                "content": question
+            })
+tokenizer = AutoTokenizer.from_pretrained(path)
+prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+input_ids = tokenizer(prompt, add_special_tokens=False, return_tensors="pt").input_ids.cuda().int()
+num_tokens = input_ids.numel()
+print(num_tokens)
+
+position_ids = torch.arange(num_tokens, dtype=torch.int32, device="cuda").view(1, num_tokens)
+teminators = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+
+if model_type == "medusa":
+    llm = W4A16LLM_with_medusa(medusa_path, path, dtype=dtype, memory_limit=0.8, medusa_num_heads=3, medusa_choices=eval('mc_sim_7b_63_3head'), cuda_graph=cuda_graph, group_size=128, use_marlin=True)
+    our_generate = lambda: llm.generate(input_ids, num_generate, teminators=teminators)
+elif model_type == "eagle":
+    llm = W4A16LLM_with_eagle(eagle_path, path, dtype=dtype, memory_limit=0.8, num_iter=6, tree_size=60, cuda_graph=cuda_graph, group_size=128, use_marlin=True)
+    our_generate = lambda: llm.generate(input_ids, num_generate, teminators=[])
+else:
+    llm = W4A16LLM(path, dtype=dtype, memory_limit=0.8, cuda_graph=cuda_graph, group_size=128, use_marlin=True)
+    our_generate = lambda: llm.generate(input_ids, num_generate, teminators=[])
+
+llm.init_storage()
+llm.load_from_hf()
+
+gen_result = our_generate()
+gen_result1 = our_generate()
+if model_type == "eagle" or model_type == "medusa":
+    import numpy as np
+    m_output = our_generate()
+    print(tokenizer.decode(m_output[0]))
+    print(len(m_output[0]))
+    print("acc:", m_output[1])
+    print("Mean acc:", np.mean(m_output[1]))
+else:
+    out_str = tokenizer.decode(gen_result)
+    print(out_str)
+    print("=========================================")
+    out_str1 = tokenizer.decode(gen_result1)
+    print(out_str1)
+    print("=========================================")
+    print(out_str1 == out_str)
+# if Bench:
+#     print("decode speed:", f"{num_generate / do_bench(our_generate, warmup=10, rep=1000) * 1000} tok/s")
