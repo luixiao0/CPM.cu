bdd2829f7fea40f9ad0851f23c3cac79fcbb8079
YudiZh <yvdi.zhang@outlook.com>
2025-05-17 17:47:06 +0800

    fix path

--- scripts/eval/mt_bench/llama3-8b-instruct/w4a8-qqq/run_baseline.sh
+++ scripts/eval/mt_bench/llama3-8b-instruct/w4a8-qqq/run_baseline.sh
@@ -6,7 +6,7 @@ Bench_name="mt_bench"
 python3 evaluation/inference_baseline_w4a8_qqq.py \
     --model-path $Model_Path \
     --cuda-graph \
-    --model-id ${Model_id}/w4a8-qoq/baseline \
+    --model-id ${Model_id}/w4a8-qqq/baseline \
     --memory-limit 0.80 \
     --bench-name $Bench_name \
     --dtype "float16"
