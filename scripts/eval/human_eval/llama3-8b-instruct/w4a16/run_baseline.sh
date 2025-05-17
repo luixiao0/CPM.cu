14f8e85d7acba43455a19630db11f3c542570272
YudiZh <yvdi.zhang@outlook.com>
2025-05-17 22:39:59 +0800

    llama-3 8b human eval scripts

---
+++ scripts/eval/human_eval/llama3-8b-instruct/w4a16/run_baseline.sh
@@ -0,0 +1,13 @@
+
+Model_Path=models/Meta-Llama-3-8B-Instruct-w4a16
+Model_id="llama-3-8b-instruct"
+Bench_name="human_eval"
+
+python3 evaluation/inference_baseline_w4a16_gptq_marlin.py \
+    --model-path $Model_Path \
+    --cuda-graph \
+    --model-id ${Model_id}/w4a16/baseline \
+    --memory-limit 0.80 \
+    --bench-name $Bench_name \
+    --dtype "float16" \
+    --max-new-tokens 512
